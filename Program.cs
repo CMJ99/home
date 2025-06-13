1f0a28395acfdec1ec4227d0ef554a2f24fb87bb
CMJ99 <jjing9ya99@gmail.com>
2025-06-14 00:36:53 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! 최민재");
+            Console.WriteLine("Add:" + Add(10, 20));
+            ValMain();
+            CritcalAttackMain();
+            StageMain();
+            AttackWile();
+            MonsterListMain();
+        }
+
+
+
+
+        static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+        static void ValMain()
+        {
+            int nScore = 0;
+            float fRat = 1.0f / 4.0f;
+            Console.WriteLine("Score:" + nScore);
+            Console.WriteLine("Rat:" + fRat);
+        }
+
+        static void CritcalAttackMain()
+        {
+            Console.WriteLine("CritcalAttackMain");
+        }
+
+        static void StageMain()
+        {
+            Console.WriteLine("StageMain");
+        }
+
+        static void AttackWile()
+        {
+            Console.WriteLine("AttackWile");
+        }
+
+        static void MonsterListMain()
+        {
+            Console.WriteLine("MonsterListMain");
+        }
+
+
+   
+    }
+
+
+
+
+}
